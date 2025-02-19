68e4b0d307696b8b75c5a642f4bcb4ef92113bb4
Gui182 <guilherme.oliveira182@yahoo.com.br>
2025-02-19 18:28:24 -0300

    feat: First Commit

---
+++ src/main/java/br/com/nlw/events/service/SubscriptionService.java
@@ -0,0 +1,46 @@
+package br.com.nlw.events.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.nlw.events.exception.EventNotFoundException;
+import br.com.nlw.events.model.Event;
+import br.com.nlw.events.model.Subscription;
+import br.com.nlw.events.model.User;
+import br.com.nlw.events.repository.EventRepo;
+import br.com.nlw.events.repository.SubscriptionRepo;
+import br.com.nlw.events.repository.UserRepo;
+
+@Service
+public class SubscriptionService {
+
+    @Autowired
+    private EventRepo eventRepo;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private SubscriptionRepo subscriptionRepo;
+
+    public Subscription createNewSubscription(String eventName, User user) {
+        Event evt = eventRepo.findByPrettyName(eventName);
+        if (evt == null) {
+            throw new EventNotFoundException("Evento " + eventName + " não existe");
+        }
+        User userRec = userRepo.findByEmail(user.getEmail());
+        if (userRec == null) {
+            user = userRepo.save(user);
+        }
+
+        user = userRepo.save(user);
+
+        Subscription subs = new Subscription();
+        subs.setEvent(evt);
+        subs.setSubscriber(userRec);
+
+        Subscription res = subscriptionRepo.save(subs);
+
+        return res;
+    }
+}
